67499d48b2920f4454966cb5ca0b03241a9a903c
Inayah Najmi Z <149597674+inayahnajmi@users.noreply.github.com>
2024-12-04 08:37:46 +0700

    Add files via upload

---
+++ 13_Repeat_Until/GUIDED/2_bilPositif.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+	var number int
+	var continueLoop bool
+	for continueLoop = true; continueLoop; {
+		fmt.Scan(&number)
+		continueLoop = number <= 0
+	}
+	fmt.Printf("%d adalah bilangan bulat positif\n", number)
+}
